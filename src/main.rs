e6b89d63d03b56a4b9968716c2af9c8917c8a5dd
bvvn1 <bvvn@protonmail.com>
2025-10-15 10:02:34 +0300

    added channels

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use rdev::{self, Event, listen};
+use std::hash;
 
-fn main() {
+use rdev::{self, Event, EventType, listen};
+use tokio::sync::mpsc;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
+async fn main() {
     //if let Err(err) = grab(callback) {
     //    println!("{:?}", err)
     //};
+
+    let (tx, rx) = mpsc::channel::<u8>(2);
 
     match listen(callback) {
         Ok(key) => println!("{:?}", key),
@@ -12,9 +18,10 @@ fn main() {
 }
 
 fn callback(event: Event) {
-    println!("My callback {:?}", event);
-    match event.name {
-        Some(string) => println!("User wrote {:?}", string),
-        None => (),
+    //println!("My callback {:?}", event);
+    match event.event_type {
+        //Some(string) => println!("User wrote {:?}", string),
+        EventType::KeyPress(keypress) => println!("{:?}", keypress),
+        _ => (),
     }
 }
